af56b64c1690288b45f314977daa41cc617b773c
Hrithik <slumdog4321@gmail.com>
2024-03-18 10:55:56 +0530

    added section divider

--- components/intro.tsx
+++ components/intro.tsx
@@ -48,7 +48,7 @@ export default function Intro() {
       </div>
 
       <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        className="mb-10 mt-4 px-4 text-xl font-medium !leading-[1.5] sm:text-2xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
