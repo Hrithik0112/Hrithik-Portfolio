5f702d3802a06a0e7932f394c6adf0f681d67467
Hrithik <slumdog4321@gmail.com>
2024-03-18 16:13:51 +0530

    context for active section

--- components/header.tsx
+++ components/header.tsx
@@ -4,8 +4,11 @@ import Link from "next/link";
 import React from "react";
 import { motion } from "framer-motion";
 import { links } from "@/lib/data";
+import clsx from "clsx";
+import { useActiveSectionContext } from "@/context/active-sectioon-context";
 
 export default function Header() {
+  const { activeSection, setActiveSection } = useActiveSectionContext();
   return (
     <header className="z-[999] relative">
       <motion.div
@@ -24,10 +27,27 @@ export default function Header() {
               animate={{ y: 0, opacity: 1 }}
             >
               <Link
-                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300"
+                className={clsx(
+                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+                  {
+                    "text-gray-950 dark:text-gray-200": activeSection === link.name,
+                  }
+                )}
                 href={link.hash}
+                onClick={() => setActiveSection(link.name)}
               >
                 {link.name}
+                {link.name === activeSection && (
+                  <motion.span
+                    className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
+                    layoutId="activeSection"
+                    transition={{
+                      type: "spring",
+                      stiffness: 380,
+                      damping: 30,
+                    }}
+                  ></motion.span>
+                )}
               </Link>
             </motion.li>
           ))}
