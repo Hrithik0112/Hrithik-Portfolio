248b05530bb712288d3f6025472e4f98745cdd13
Hrithik <slumdog4321@gmail.com>
2024-03-18 23:24:00 +0530

    contact section using Resend

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body
-        className={`${inter.className} relative bg-gray-50 text-gray-950 h-[6000px] pt-28 sm:pt-36`}
-      >
+      <body className={`${inter.className} relative bg-gray-50 text-gray-950  pt-28 sm:pt-36`}>
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
         <ActiveSectionContextProvider>
